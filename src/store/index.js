2a4fc8ee99e83b3d2dd9d87b4aec70b98771c02d
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2021-08-21 21:55:01 +0500

    add redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,7 @@ import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"
 import userIdReducer from "./reducers/userIdReducer";
 import logger from 'redux-logger';
+import thunk from "redux-thunk";
 
 const persistConfig = {
     key: "root",
@@ -15,7 +16,7 @@ const rootReducer = combineReducers({
 });
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(logger)));
+const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(logger, thunk)));
 
 export const persistor = persistStore(store);
 export default store;
