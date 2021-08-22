7949e1d81f2ab5a26154ce3441b046abc1dd0bda
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2021-08-22 23:27:19 +0500

    set many component

--- src/containers/SignUp.js
+++ src/containers/SignUp.js
@@ -1,150 +1,177 @@
 import React, { useState } from "react";
 import { Row, Col, Form, Input, Button } from "antd";
-import { auth } from '../configs/firebase';
+import { auth, db } from '../configs/firebase';
 import "../style/signup.css";
 import { useDispatch } from "react-redux";
 import { addUserID } from "../store/actions/userIdAction";
 import { useHistory } from "react-router-dom";
+import {MdPhotoCamera} from "react-icons/all";
 
 export default function SignUp () {
-    
     const dispatch = useDispatch();
     const history = useHistory();
-    const [state, setState] = useState({
+    const [user, setUser] = useState({
+        displayName: "",
+        phoneNumber: "",
         email: '',
         password: '',
     });
     const [errorMsg, setErrorMsg] = useState({
         code: "",
-        massega: "",
+        message: "",
     });
+    const [image, setImage] = useState();
+    const [url, setUrl] = useState("https://firebasestorage.googleapis.com/v0/b/dentist-test-app.appspot.com/o/placeholder-image.jpg?alt=media&token=9da37c26-a05f-4bad-a3a7-c8ce94c57044");
+
+    const chooseImageHendler = (e) => {
+        const reader = new FileReader();
+        reader.onload = () =>{
+          if(reader.readyState === 2){
+            setUrl(reader.result);
+          }
+        }
+        reader.readAsDataURL(e.target.files[0])
+      };
 
     const hendlerInputChange = (e) => {
         const {name, value} = e.target;
-        setState((prevState) => ({
+        setUser((prevState) => ({
             ...prevState, [name]: value
         }));
-        console.log(state);
+        console.log(user);
     }
 
-    const handleLogin = async () => {
-        try {
-          const { email, password } = state;
-          const authenticatedUser = await auth.signInWithEmailAndPassword(email, password);
-          console.log(authenticatedUser);
-          dispatch(addUserID(authenticatedUser));
-          history.push("/balance");
-        } catch (error) {
-          console.log(error);
-          setErrorMsg({ code: error.code, message: error.message })
-        }
+    const handleLogin = (e) => {
+      e.preventDefault();
+          const { email, password } = user;
+          auth.createUserWithEmailAndPassword(email, password)
+          .then((userCredential) => { 
+            var user = userCredential.user;
+            auth.onAuthStateChanged
+            
+          })
+          .catch((error) => {
+            console.log(error);
+            setErrorMsg({code: error.code, massega: error.message});
+            console.log(errorMsg);
+          });
       } 
-      const {email, password} = state;
 
+      const {email, password, displayName, phoneNumber} = user;
     return(
         <div>
-            <Row>
-                <Col className="sign-up-image" span={12}></Col>
-                <Col className="sign-in-right" span={12}>
-                    <Row className="sign-in-form" justify="center "  align="middle" >
-                        <Col span={12}>
-                        <Form
-                            layout="vertical"
-                            name="basic"
-                            labelCol={{
-                              span: 24,
-                            }}
-                            wrapperCol={{
-                              span: 24,
-                            }}
-                            initialValues={{
-                              remember: true,
-                            }}
-                        >
-                            <Form.Item
-                              label="EMAIL"
-                              name="username"
-                              rules={[
-                                {
-                                  required: false,
-                                  message: 'Please input your username!',
-                                },
-                              ]}
-                            >
-                              <Input
-                               name="email"
-                               value={state.email}
-                               onChange={hendlerInputChange} 
-                              />
-                            </Form.Item>
-                      
-                            <Form.Item
-                              label="PASSWORD"
-                              name="password"
-                              rules={[
-                                {
-                                  required: false,
-                                  message: 'Please input your password!',
-                                },
-                              ]}
-                            >
-                              <Input.Password
-                                    name="password"
-                                    value={state.password}
-                                    onChange={hendlerInputChange} 
-                              />
-                            </Form.Item>
-                            
-                            <Form.Item
-                              label="PASSWORD"
-                              name="password"
-                              rules={[
-                                {
-                                  required: false,
-                                  message: 'Please input your password!',
-                                },
-                              ]}
-                            >
-                              <Input.Password
-                                    name="password"
-                                    value={state.password}
-                                    onChange={hendlerInputChange} 
-                              />
-                            </Form.Item>
-
-                            <Form.Item
-                              label="PASSWORD"
-                              name="password"
-                              rules={[
-                                {
-                                  required: false,
-                                  message: 'Please input your password!',
-                                },
-                              ]}
-                            >
-                              <Input.Password
-                                    name="password"
-                                    value={state.password}
-                                    onChange={hendlerInputChange} 
-                              />
-                            </Form.Item>
-                      
-                            <Form.Item
-                              wrapperCol={{
-                                offset: 8,
-                                justify: "center",
-                                span: 16,
-                              }}
-                            >
-                              <Button disabled={!(email) && !(password)} onClick={handleLogin}>
-                                Submit
-                              </Button>
-                            </Form.Item>
-                          </Form>
-                        </Col>
-                    </Row>
+          <Row>
+            <Col className="sign-up-image" span={12}></Col>
+            <Col className="sign-in-right" span={12} justify="center"  align="middle" >           
+              <Row className="sign-up-form">
+                <Col className="user-img" span={14} style={{alignSelf: "flex-end"}}>
+                  <div style={{backgroundImage: `url(${url})`}} className="user-photo">
+                    <span className="file-span">
+                      <label className="file-label" htmlFor="file"> <MdPhotoCamera /></label>
+                      <input className="photo-file" onChange={chooseImageHendler} name="file" id="file" type="file"></input>
+                    </span>
+                  </div>
                 </Col>
-            </Row>
+               <Col span={14} style={{alignSelf: "flex-start"}} >
+                  <Form
+                      layout="vertical"
+                      name="basic"
+                      labelCol={{
+                        span: 24,
+                      }}
+                      wrapperCol={{
+                        span: 24,
+                      }}
+                      initialValues={{
+                        remember: true,
+                      }}
+                  >
+                      <Form.Item
+                        label="Display Name"
+                        name="displayName"
+                        rules={[
+                          {
+                            required: false,
+                            message: 'Please input your username!',
+                          },
+                        ]}
+                      >
+                        <Input
+                         name="displayName"
+                         value={user.displayName}
+                         onChange={hendlerInputChange} 
+                         addonBefore="@"
+                        />
+                      </Form.Item>
+                
+                      <Form.Item
+                        name="phoneNumber"
+                        label="Phone Number"
+                        rules={[
+                          {
+                            required: false,
+                            message: 'Please input your password!',
+                          },
+                        ]}
+                      >
+                        <Input
+                              name="phoneNumber"
+                              value={user.phoneNumber}
+                              onChange={hendlerInputChange} 
+                        />
+                      </Form.Item>
+                            
+                      <Form.Item
+                        label="Email"
+                        name="email"
+                        rules={[
+                          {
+                            required: false,
+                            message: 'Please input your password!',
+                          },
+                        ]}
+                      >
+                        <Input
+                              name="email"
+                              type="email"
+                              value={user.email}
+                              onChange={hendlerInputChange} 
+                        />
+                      </Form.Item>
+
+                      <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[
+                          {
+                            required: false,
+                            message: 'Please input your password!',
+                          },
+                        ]}
+                      >
+                        <Input.Password
+                              name="password"
+                              value={user.password}
+                              onChange={hendlerInputChange} 
+                        />
+                      </Form.Item>
+                      
+                      <Form.Item
+                        wrapperCol={{
+                          offset: 4,
+                          justify: "center",
+                          span: 16,
+                        }}
+                      >
+                        <Button disabled={!(email) || !(password) || !(phoneNumber) || !(displayName)} onClick={handleLogin}>
+                          Submit
+                        </Button>
+                      </Form.Item>
+                    </Form>
+                  </Col>
+                </Row>
+            </Col>
+          </Row>
         </div>
     );
 }
